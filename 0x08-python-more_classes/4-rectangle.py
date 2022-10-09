b1201e20817455f072bed9864b4566c7afc68f95
radu2022 <seid.hamid2@gmail.com>
2022-10-09 07:51:41 -0700

    pyrhonmoreclasses

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Create a Rectangle class."""
+
+
+class Rectangle:
+    """Definition a class rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initializing Rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not type(int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        """Print the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ((("#" * self.width) + "\n") * self.height)[:-1]
+
+    def __repr__(self):
+        """Print the rectangle using eval."""
+        return "Rectangle({}, {})".format(self.width, self.height)
---
+++ 0x08-python-more_classes/tests/1-main.py
@@ -0,0 +1,8 @@
+ectangle = __import__('1-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
